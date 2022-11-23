229e8a27ec7ce1cb698f0f0960de9feabe0f1722
LucaFreire <lucasrodifreire@gmail.com>
2022-11-23 11:52:08 -0300

    Aula3 Modelos Conceituais

--- Aula2.sql
+++ Aula2.sql
@@ -9,6 +9,7 @@ create table tabela1(
   peso numeric(4,2) -- (total numeros, quantos depois da vírgula) 
 )
 
+
 INSERT INTO tabela1(nome,sobrenome,cpf,estado,dataNasc,altura,peso)
 VALUES
 ('Nome2','Sobrenome2','123456789','PR','15/10/2010', 1.73, 7.7); -- MES / DIA / ANO mas parace como ANO / MES / DIA
@@ -35,6 +36,7 @@ Select * from tabela1 where peso between 20 and 90 -- Todos que contém o peso en
 
 ----------------------------------------------------------------------------------------------------
 
+/*
 create table unidade(
 	id int identity (1,1) primary key,
 	nome varchar(30)
@@ -50,9 +52,11 @@ create table tabela2(
 	nome varchar(30),
 	id_unidade int,
 	foreign key (id_unidade) references tabela1(id)
-	
 	)
+*/
+
 ------------------------------------------------------
+/*
 create table palestras(
 	id int identity(1,1) primary key,
 	nome varchar(30),
@@ -74,11 +78,12 @@ add constraibt PK_inspetir primary key(id)
 
 
 alter table inspetor
-add constraint PK_inspetor primary key(id_palestra) references palestras(id)
+add constraint PK_inspetor primary key(id_palestra) references palestras(id) */
 
 
 ------------------------------------------------------------------------------------------
 
+--------------------- Exercício --------------------
 create table Cliente(
 	Id int identity(1,1) primary key,
 	Nome varchar(30),
@@ -102,6 +107,32 @@ create table Venda(
 	foreign key(IdProduto) references Produto(Id)
 )
 
+INSERT INTO Cliente(Nome,Cpf)
+VALUES
+	('Thigas','11111111111'),
+	('Pedro','11111111222'),
+	('Roberto','11111111333');
+
+INSERT INTO Produto
+VALUES
+	('Água', 5.00),
+	('Chocolate',7.50),
+	('Salgado', 10.00),
+	('Refrigerante',12.50),
+	('Café',19.90);
+
+INSERT INTO Venda(IdCliente, IdProduto)
+Values 
+	(1,3),(2,4),(3,5)
+
+alter table Venda
+add constraint fk_cliente foreign key (IdCliente) -- Com essa alteração, agora é possível deletar vendas sem que de erro
+references Cliente(Id) on update cascade on delete cascade
 
 
-)
+-- SELECT <dado1,dado2,dado3> FROM // Tudo que estiver entre o SELECT e o FROM, vai aparecer
+SELECT c.Nome, p.Nome AS Produto, p.Preco FROM VENDA AS v
+	JOIN Cliente AS c on c.id = v.IdCliente
+	JOIN Produto AS p on p.id = v.IdProduto
+	where(p.Preco >= 10)
+
